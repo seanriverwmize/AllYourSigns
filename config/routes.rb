a979d64fbb96ba4539f20eb1077e71d86f4b66fa
Sean River <seanriverwmize@gmail.com>
2019-02-10 17:04:12 -0800

    rerouted the home page to welcome#about. added some bootstrap styling to links

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :subjects
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
-  get 'about', to: 'welcome#about', as: 'about' 
+  root 'welcome#about'
+  get 'about', to: 'welcome#about', as: 'about'
 end
